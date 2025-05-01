fd62c728872ea5c4ca825f1c2ce42c70125c24f1
johnykosmos <piotrpomaranski97@gmail.com>
2025-05-01 10:26:41 +0200

    Feat: Modularize updateChunks() method

--- src/world/ChunkManager.cpp
+++ src/world/ChunkManager.cpp
@@ -8,9 +8,46 @@ const CordChunkMap& ChunkManager::getLoadedChunks() const {
     return loadedChunks;
 }
 
+bool ChunkManager::loadMissingChunks(std::vector<ChunkCords>& missingChunks) {
+    bool updated = false;
+    for (const auto& missingCords : missingChunks) {
+        auto it = loadedChunks.find(missingCords);
+        if (it == loadedChunks.end()) {
+            loadedChunks[missingCords] = std::make_unique<Chunk>(missingCords);
+            loadedChunks[missingCords]->buildMesh();
+            updated = true;
+            std::cout << "Loaded a chunk at: " << missingCords.x << " " << missingCords.z << "\n";
+        }
+    }
+    return updated;
+}
+
+
+bool ChunkManager::unloadNotUsedChunks(std::vector<ChunkCords>& neededChunks) {
+    bool updated = false;
+    std::vector<ChunkCords> toErase;
+    for (const auto& [cords, chunk] : loadedChunks) {
+        bool found = false;
+        for (const auto& neededCords : neededChunks) {
+            if (neededCords == cords) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            toErase.push_back(cords);
+            updated = true;
+        }
+    }
+
+    for (const auto& cords : toErase) {
+        loadedChunks.erase(cords);
+    }
+    return updated;
+}
+
 bool ChunkManager::updateChunks(Vec3 playerPosition, 
         int renderDistance) {
-    bool updated = false;
     std::vector<ChunkCords> wantedChunks;
 
     for (int x = -renderDistance; x < renderDistance; x++) {
@@ -21,37 +58,12 @@ bool ChunkManager::updateChunks(Vec3 playerPosition,
             };
 
             wantedChunks.push_back(wantedCords);
-            auto it = loadedChunks.find(wantedCords);
-            if (it == loadedChunks.end()) {
-                loadedChunks[wantedCords] = std::make_unique<Chunk>(wantedCords);
-                loadedChunks[wantedCords]->buildMesh();
-                updated = true;
-                std::cout << "Loaded a chunk at: " << wantedCords.x << " " << wantedCords.z << "\n";
-            } 
         }
     }
 
-    if (updated) {
-        std::vector<ChunkCords> toErase;
+    bool loaded = loadMissingChunks(wantedChunks);
+    bool unloaded = unloadNotUsedChunks(wantedChunks);
 
-        for (const auto& [cords, chunk] : loadedChunks) {
-            bool found = false;
-            for (const auto& wanted : wantedChunks) {
-                if (wanted == cords) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                toErase.push_back(cords);
-            }
-        }
-
-        for (const auto& cords : toErase) {
-            loadedChunks.erase(cords);
-        }
-    }
-
-    return updated;
+    return loaded || unloaded;
 }
 
--- src/world/ChunkManager.hpp
+++ src/world/ChunkManager.hpp
@@ -10,6 +10,10 @@ class ChunkManager {
     private:
         CordChunkMap loadedChunks;
 
+    private: 
+        bool loadMissingChunks(std::vector<ChunkCords>& missingChunks);
+        bool unloadNotUsedChunks(std::vector<ChunkCords>& neededChunks);
+
     public:
         const CordChunkMap& getLoadedChunks() const;
         bool updateChunks(Vec3 playerPosition, int renderDistance);
