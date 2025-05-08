ecc6f9627b6064a4d5f69ade422f507a5e5b2d80
johnykosmos <piotrpomaranski97@gmail.com>
2025-05-08 20:52:49 +0200

    Feat: Add BlockAtlas to store and manage general BlockData

---
+++ src/world/BlockAtlas.cpp
@@ -0,0 +1,83 @@
+#include "BlockAtlas.hpp"
+
+std::map<BlockID, BlockData> BlockAtlas::blockMap = {
+    {
+        BlockID::Dirt, 
+        {
+            .topUV = Vec2{0, 0}, 
+            .bottomUV = Vec2{0, 0},
+            .frontUV = Vec2{0, 0},
+            .backUV = Vec2{0, 0},
+            .leftUV = Vec2{0, 0},
+            .rightUV = Vec2{0, 0},
+        }
+    },
+    {
+        BlockID::Grass, 
+        {
+            .topUV = Vec2{2, 0}, 
+            .bottomUV = Vec2{0, 0},
+            .frontUV = Vec2{1, 0},
+            .backUV = Vec2{1, 0},
+            .leftUV = Vec2{1, 0},
+            .rightUV = Vec2{1, 0},
+        }
+    },
+    {
+        BlockID::Stone, 
+        {
+            .topUV = Vec2{1, 1}, 
+            .bottomUV = Vec2{1, 1},
+            .frontUV = Vec2{1, 1},
+            .backUV = Vec2{1, 1},
+            .leftUV = Vec2{1, 1},
+            .rightUV = Vec2{1, 1},
+        }
+    },
+    {
+        BlockID::Sand, 
+        {
+            .topUV = Vec2{0, 1}, 
+            .bottomUV = Vec2{0, 1},
+            .frontUV = Vec2{0, 1},
+            .backUV = Vec2{0, 1},
+            .leftUV = Vec2{0, 1},
+            .rightUV = Vec2{0, 1},
+        }
+    }
+
+};
+
+bool BlockAtlas::doesBlockExist(BlockID block) {
+    auto it = blockMap.find(block);
+    return (it != blockMap.end());
+}
+
+const Vec2 BlockAtlas::getFaceTextureUV(BlockID block, Vec3 faceNormal) {
+    const Vec2 nullUV = {0,0};
+    if (doesBlockExist(block)) {
+        auto it = blockMap.find(block);
+        const BlockData blockData = it->second;
+        
+        if (faceNormal.x == 1.0f) {
+            return blockData.rightUV;
+        } 
+        if (faceNormal.x == -1.0f) {
+            return blockData.leftUV;
+        } 
+        if (faceNormal.y == 1.0f) {
+            return blockData.topUV;
+        }
+        if (faceNormal.y == -1.0f) {
+            return blockData.bottomUV;
+        }
+        if (faceNormal.z == 1.0f) {
+            return blockData.frontUV;
+        }
+        if (faceNormal.z == -1.0f) {
+            return blockData.backUV;
+        }
+    }
+    return nullUV;
+}
+
---
+++ src/world/BlockAtlas.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Typedefs.hpp"
+#include <map>
+
+
+enum class BlockID : char {
+    Air = 0,
+    Dirt = 1,
+    Grass = 2,
+    Stone = 3,
+    Sand = 4
+};
+
+struct BlockData {
+    const Vec2 topUV;
+    const Vec2 bottomUV;
+    const Vec2 frontUV;
+    const Vec2 backUV;
+    const Vec2 leftUV;
+    const Vec2 rightUV;
+};
+
+class BlockAtlas {
+    private:
+        static std::map<BlockID, BlockData> blockMap; 
+
+    private:
+        static bool doesBlockExist(BlockID block);
+
+    public:
+        static const Vec2 getFaceTextureUV(BlockID block, Vec3 faceNormal);
+
+};
